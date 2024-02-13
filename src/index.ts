8c787c78371b4897950be61b3a125f958c45d9c4
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-02-14 00:25:17 +0700

    feat: add googlemaps api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello")
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User()
+console.log(user)
+
+const company = new Company()
+console.log(company)
